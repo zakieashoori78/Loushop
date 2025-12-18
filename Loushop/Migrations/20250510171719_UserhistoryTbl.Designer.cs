1c3c6b1db68e305f2c9c57dcef73569f67c4ea0a
zakie78.ashoori@gmail.com <zakie78.ashoori@gmail.com>
2025-12-18 15:16:01 +0330

    optimize style

--- Loushop/Migrations/20250510171719_UserhistoryTbl.Designer.cs
+++ Loushop/Migrations/20250510171719_UserhistoryTbl.Designer.cs
@@ -4,6 +4,7 @@ using Loushop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Loushop.Migrations
 {
     [DbContext(typeof(LouShopContext))]
-    partial class LouShopContextModelSnapshot : ModelSnapshot
+    [Migration("20250510171719_UserhistoryTbl")]
+    partial class UserhistoryTbl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -289,25 +292,6 @@ namespace Loushop.Migrations
                             ItemId = 3,
                             Name = "دمپایی ابری"
                         });
-                });
-
-            modelBuilder.Entity("Loushop.Models.UserHistory", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<DateTime>("CreatDate")
-                        .HasColumnType("datetime2");
-
-                    b.Property<string>("UserId")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("UserHistories");
                 });
 
             modelBuilder.Entity("Loushop.Models.Users", b =>
---
+++ Loushop/Migrations/20250510172837_Userhistorytable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loushop.Migrations
+{
+    /// <inheritdoc />
+    public partial class Userhistorytable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserHistories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserHistories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserHistories");
+        }
+    }
+}
